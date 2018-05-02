9427a92fc79ad856343a19dee3e4ded9dcea776b
Ivan Kryvoruchko <gildraug@gmail.com>
2018-05-02 13:54:43 +0300

    Add structopt-based CLI arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@ extern crate failure;
 #[macro_use]
 extern crate structopt;
 
+use std::num::ParseIntError;
+
 use failure::Error;
 
 use structopt::StructOpt;
 
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(name = "GAME")]
+    game: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
